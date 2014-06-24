3ca2b20791761867b2dfd2b600b52e99bbb96bb8
Tommy Fan <lecheng.fan@stanford.edu>
2014-06-23 23:25:46 -0600

    created django app

---
+++ soundscape_project/soundmap/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
+def index(request):
+	context = RequestContext(request)
+
+	return render_to_response('soundmap/index.html', {}, context)
