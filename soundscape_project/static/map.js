76da92de8163f747ff05abc41373344fc1d8bd28
Ian Char <ianchar4@gmail.com>
2014-06-24 14:38:04 -0600

    Made the basic layout of the main screen

---
+++ soundscape_project/static/map.js
@@ -0,0 +1,24 @@
+var map;
+
+function initialize() {
+	var mapOptions = {
+
+	  center: new google.maps.LatLng(10, 0),
+	  zoom: 3
+	};
+	map = new google.maps.Map(document.getElementById("map-canvas"),
+	    mapOptions);
+}
+
+// $(document).ready(function(){
+// 	//$('#map-canvas').css("heigh", $(window).height());
+// 	//$('#map-canvas').css("width", $(window).width());
+// 	google.maps.event.trigger(map, 'resize');
+// });
+
+google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, "resize", function() {
+ var center = map.getCenter();
+ google.maps.event.trigger(map, "resize");
+ map.setCenter(center); 
+});
