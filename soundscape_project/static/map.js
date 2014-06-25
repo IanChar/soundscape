8a99a482058c74d174b9cd22a88348a921595314
Ian Char <ianchar4@gmail.com>
2014-06-25 12:03:07 -0600

    Can now place a marker or get current location

--- soundscape_project/static/map.js
+++ soundscape_project/static/map.js
@@ -1,18 +1,48 @@
 
-//*******INITIALIZING THE MAP***********
+//***********************MAP STUFF****************************
 var map;
+var canPlaceMarker = false;
+var currentLocation;
+var browserSupportFlag = new Boolean();
 
 function initialize() {
 	
-
+	//*****************MAP OPTIONS*******************************
 	var mapOptions = {
-
 	  center: new google.maps.LatLng(10, 0),
 	  zoom: 2
 	};
 	map = new google.maps.Map(document.getElementById("map-canvas"),
 	    mapOptions);
 
+	//******************GET CURRENT LOCATION*********************
+	 // Try W3C Geolocation (Preferred)
+	  if(navigator.geolocation) {
+	    browserSupportFlag = true;
+	    navigator.geolocation.getCurrentPosition(function(position) {
+	      currentLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+	    }, function() {
+	      handleNoGeolocation(browserSupportFlag);
+	    });
+	  }
+	  // Browser doesn't support Geolocation
+	  else {
+	    browserSupportFlag = false;
+	    handleNoGeolocation(browserSupportFlag);
+	  }
+
+	  function handleNoGeolocation(errorFlag) {
+	    if (errorFlag == true) {
+	      alert("Geolocation service failed.");
+	      initialLocation = newyork;
+	    } else {
+	      alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
+	      initialLocation = siberia;
+	    }
+	    map.setCenter(initialLocation);
+	  }
+
+	//****************LISTENERS*********************************
 	google.maps.event.addListener(map, 'click', function(event) {
 	   placeMarker(event.latLng);
 	});
@@ -26,15 +56,27 @@ google.maps.event.addDomListener(window, "resize", function() {
 });
 
 
-//*******Add Markers*******************
-
-
-function placeMarker(location) {
-    var marker = new google.maps.Marker({
-        position: location, 
-        map: map
-    });
+//***********************MARKER FUCNTIONS********************
+var placeMarker = function(location) {
+	if(canPlaceMarker)
+	{    
+	    var marker = new google.maps.Marker({
+	        position: location, 
+	        map: map
+	    });
+	    canPlaceMarker = false;
+	}
 }
 
+var enablePlacing = function() {
+	canPlaceMarker = true;
+}
+
+var placeCurrentLocationMarker = function() {
+	canPlaceMarker = true;
+	placeMarker(currentLocation);
+}
+
+//********************INITIALIZE THE MAP*********************
 google.maps.event.addDomListener(window, 'load', initialize);
 
