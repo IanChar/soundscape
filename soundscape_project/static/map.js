e076a5cc4c211e5f36873b4d21fe35be050cc2c6
Ian Char <ianchar4@gmail.com>
2014-06-24 21:30:01 -0600

    Now when you click somewhere on a map a marker is added

--- soundscape_project/static/map.js
+++ soundscape_project/static/map.js
@@ -1,24 +1,40 @@
+
+//*******INITIALIZING THE MAP***********
 var map;
 
 function initialize() {
+	
+
 	var mapOptions = {
 
 	  center: new google.maps.LatLng(10, 0),
-	  zoom: 3
+	  zoom: 2
 	};
 	map = new google.maps.Map(document.getElementById("map-canvas"),
 	    mapOptions);
+
+	google.maps.event.addListener(map, 'click', function(event) {
+	   placeMarker(event.latLng);
+	});
 }
 
-// $(document).ready(function(){
-// 	//$('#map-canvas').css("heigh", $(window).height());
-// 	//$('#map-canvas').css("width", $(window).width());
-// 	google.maps.event.trigger(map, 'resize');
-// });
+//Stuff to do with the window
+google.maps.event.addDomListener(window, "resize", function() {
+	var center = map.getCenter();
+	google.maps.event.trigger(map, "resize");
+	map.setCenter(center); 
+});
+
+
+//*******Add Markers*******************
+
+
+function placeMarker(location) {
+    var marker = new google.maps.Marker({
+        position: location, 
+        map: map
+    });
+}
 
 google.maps.event.addDomListener(window, 'load', initialize);
-google.maps.event.addDomListener(window, "resize", function() {
- var center = map.getCenter();
- google.maps.event.trigger(map, "resize");
- map.setCenter(center); 
-});
+
