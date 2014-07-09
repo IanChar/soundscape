caf8c74c73629cc5c13939299e7530947c66e2e6
Ian Char <ianchar4@gmail.com>
2014-07-08 23:41:57 -0600

    Parsed url for artist and title so they do not need to be stored

--- soundscape_project/soundmap/ajax.py
+++ soundscape_project/soundmap/ajax.py
@@ -1,12 +1,36 @@
 from django.utils import simplejson
 from dajaxice.decorators import dajaxice_register
 
+def findArtist(url):
+	stop = len(url)
+	for i in range(23, len(url)):
+		if url[i] == '/':
+			stop = i
+			break
+	return makePretty(url[23:stop])
+
+def findTitle(url):
+	start = 0
+	for i in range(len(url) - 2, 0, -1):
+		if url[i] == '/':
+			start = i
+			break
+	return makePretty(url[start + 1:len(url) - 1])
+
+def makePretty(url):
+	url = url.replace('-', ' ')
+	url = url.title()
+	return url
+
+
+
 @dajaxice_register
 def getMarkerInfo(request):
+	sampleUrl = "https://soundcloud.com/shumbody/folds-in-your-hands/"
 	marker1 = {'artist':"2PAC", 'title':"Changes", 'lat':10, 'lng':20, 'url':"https://soundcloud.com/ghetto-records-2/2pac-changes"}
 	marker2 = {'artist':"Borat", 'title':"Throw the Jew Down the Well", 'lat':48, 'lng':68, 'url':"https://soundcloud.com/user3843991/borat-throw-the-jew-down-the"}
 	marker3 = {'artist':"Passion Pit", 'title':"It's Time", 'lat':42, 'lng':-71, 'url':"https://soundcloud.com/imaginedragons/imagine-dragons-its-time-2"}
-	marker4 = {'artist':"Shumbody", 'title':"Folds in your Hands", 'lat':37, 'lng':-120, 'url':"https://soundcloud.com/shumbody/folds-in-your-hands/"}
+	marker4 = {'artist':findArtist(sampleUrl), 'title':findTitle(sampleUrl), 'lat':37, 'lng':-120, 'url':sampleUrl}
 	marker5 = {'artist':"Teriyaki Boyz", 'title':"Tokyo Drift", 'lat':35, 'lng':136, 'url':"https://soundcloud.com/jdm-music-2/teriyaki-boyz-tokyo-drift"}
 	
 	return simplejson.dumps({'m1':marker1,'m2':marker2, 'm3':marker3, 'm4':marker4, 'm5':marker5})
