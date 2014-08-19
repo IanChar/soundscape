54b313781735b0dcf494e1b8c8f73525e15d62c3
Lecheng <lecheng.fan@stanford.edu>
2014-08-18 23:15:19 -0600

    heroku upload

--- soundscape_project/soundscape_project/wsgi.py
+++ soundscape_project/soundscape_project/wsgi.py
@@ -31,7 +31,7 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'soundscape_project.settings'
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
 
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+#from django.core.wsgi import get_wsgi_application
+#from dj_static import Cling
 
-application = Cling(get_wsgi_application)
+#application = Cling(get_wsgi_application)
